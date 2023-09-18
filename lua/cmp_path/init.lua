4997a46ac7499f6aff69fd114d170ff6832005a2
Daniil Shvalov <shvalovich@gmail.com>
2023-09-19 02:23:11 +0300

    refactor!: always trigger path

--- lua/cmp_path/init.lua
+++ lua/cmp_path/init.lua
@@ -1,7 +1,4 @@
-local cmp = require 'cmp'
-
-local NAME_REGEX = '\\%([^/\\\\:\\*?<>\'"`\\|]\\)'
-local PATH_REGEX = vim.regex(([[\%(\%(/PAT*[^/\\\\:\\*?<>\'"`\\| .~]\)\|\%(/\.\.\)\)*/\zePAT*$]]):gsub('PAT', NAME_REGEX))
+local cmp = require("cmp")
 
 local source = {}
 
@@ -13,26 +10,19 @@ local constants = {
 ---@field public trailing_slash boolean
 ---@field public label_trailing_slash boolean
 ---@field public get_cwd fun(): string
-
+-- lua/option\ with\ spaces/some
 ---@type cmp_path.Option
 local defaults = {
   trailing_slash = false,
   label_trailing_slash = true,
+  filename_regex = [[[^%s{}%[%]"']+$]],
   get_cwd = function(params)
-    return vim.fn.expand(('#%d:p:h'):format(params.context.bufnr))
+    return vim.fn.expand(("#%d:p:h"):format(params.context.bufnr))
   end,
 }
 
 source.new = function()
   return setmetatable({}, { __index = source })
-end
-
-source.get_trigger_characters = function()
-  return { '/', '.' }
-end
-
-source.get_keyword_pattern = function(self, params)
-  return NAME_REGEX .. '*'
 end
 
 source.complete = function(self, params, callback)
@@ -43,7 +33,11 @@ source.complete = function(self, params, callback)
     return callback()
   end
 
-  local include_hidden = string.sub(params.context.cursor_before_line, params.offset, params.offset) == '.'
+  local include_hidden = string.sub(
+    params.context.cursor_before_line,
+    params.offset,
+    params.offset
+  ) == "."
   self:_candidates(dirname, include_hidden, option, function(err, candidates)
     if err then
       return callback()
@@ -54,7 +48,7 @@ end
 
 source.resolve = function(self, completion_item, callback)
   local data = completion_item.data
-  if data.stat and data.stat.type == 'file' then
+  if data.stat and data.stat.type == "file" then
     local ok, documentation = pcall(function()
       return self:_get_documentation(data.path, constants.max_lines)
     end)
@@ -66,72 +60,35 @@ source.resolve = function(self, completion_item, callback)
 end
 
 source._dirname = function(self, params, option)
-  local s = PATH_REGEX:match_str(params.context.cursor_before_line)
-  if not s then
-    return nil
+  local filename =
+    params.context.cursor_before_line:match(option.filename_regex)
+  if not filename or #filename == 0 then
+    return
   end
 
-  local dirname = string.gsub(string.sub(params.context.cursor_before_line, s + 2), '%a*$', '') -- exclude '/'
-  local prefix = string.sub(params.context.cursor_before_line, 1, s + 1) -- include '/'
+  if filename:sub(1, 1) == "/" then
+    filename = vim.fs.normalize(filename)
+  else
+    filename = vim.fs.joinpath(option.get_cwd(params), filename)
+  end
 
-  local buf_dirname = option.get_cwd(params)
-  if vim.api.nvim_get_mode().mode == 'c' then
-    buf_dirname = vim.fn.getcwd()
-  end
-  if prefix:match('%.%./$') then
-    return vim.fn.resolve(buf_dirname .. '/../' .. dirname)
-  end
-  if (prefix:match('%./$') or prefix:match('"$') or prefix:match('\'$')) then
-    return vim.fn.resolve(buf_dirname .. '/' .. dirname)
-  end
-  if prefix:match('~/$') then
-    return vim.fn.resolve(vim.fn.expand('~') .. '/' .. dirname)
-  end
-  local env_var_name = prefix:match('%$([%a_]+)/$')
-  if env_var_name then
-    local env_var_value = vim.fn.getenv(env_var_name)
-    if env_var_value ~= vim.NIL then
-      return vim.fn.resolve(env_var_value .. '/' .. dirname)
-    end
-  end
-  if prefix:match('/$') then
-    local accept = true
-    -- Ignore URL components
-    accept = accept and not prefix:match('%a/$')
-    -- Ignore URL scheme
-    accept = accept and not prefix:match('%a+:/$') and not prefix:match('%a+://$')
-    -- Ignore HTML closing tags
-    accept = accept and not prefix:match('</$')
-    -- Ignore math calculation
-    accept = accept and not prefix:match('[%d%)]%s*/$')
-    -- Ignore / comment
-    accept = accept and (not prefix:match('^[%s/]*$') or not self:_is_slash_comment())
-    if accept then
-      return vim.fn.resolve('/' .. dirname)
-    end
-  end
-  return nil
+  return vim.fs.dirname(filename)
 end
 
 source._candidates = function(_, dirname, include_hidden, option, callback)
-  local fs, err = vim.loop.fs_scandir(dirname)
-  if err then
-    return callback(err, nil)
-  end
-
   local items = {}
 
   local function create_item(name, fs_type)
-    if not (include_hidden or string.sub(name, 1, 1) ~= '.') then
+    if not (include_hidden or string.sub(name, 1, 1) ~= ".") then
       return
     end
 
-    local path = dirname .. '/' .. name
+    local path = dirname .. "/" .. name
     local stat = vim.loop.fs_stat(path)
     local lstat = nil
     if stat then
       fs_type = stat.type
-    elseif fs_type == 'link' then
+    elseif fs_type == "link" then
       -- Broken symlink
       lstat = vim.loop.fs_lstat(dirname)
       if not lstat then
@@ -153,14 +110,14 @@ source._candidates = function(_, dirname, include_hidden, option, callback)
         lstat = lstat,
       },
     }
-    if fs_type == 'directory' then
+    if fs_type == "directory" then
       item.kind = cmp.lsp.CompletionItemKind.Folder
       if option.label_trailing_slash then
-        item.label = name .. '/'
+        item.label = name .. "/"
       else
         item.label = name
       end
-      item.insertText = name .. '/'
+      item.insertText = name .. "/"
       if not option.trailing_slash then
         item.word = name
       end
@@ -168,14 +125,7 @@ source._candidates = function(_, dirname, include_hidden, option, callback)
     table.insert(items, item)
   end
 
-  while true do
-    local name, fs_type, e = vim.loop.fs_scandir_next(fs)
-    if e then
-      return callback(fs_type, nil)
-    end
-    if not name then
-      break
-    end
+  for name, fs_type in vim.fs.dir(dirname) do
     create_item(name, fs_type)
   end
 
@@ -183,30 +133,30 @@ source._candidates = function(_, dirname, include_hidden, option, callback)
 end
 
 source._is_slash_comment = function(_)
-  local commentstring = vim.bo.commentstring or ''
-  local no_filetype = vim.bo.filetype == ''
+  local commentstring = vim.bo.commentstring or ""
+  local no_filetype = vim.bo.filetype == ""
   local is_slash_comment = false
-  is_slash_comment = is_slash_comment or commentstring:match('/%*')
-  is_slash_comment = is_slash_comment or commentstring:match('//')
+  is_slash_comment = is_slash_comment or commentstring:match("/%*")
+  is_slash_comment = is_slash_comment or commentstring:match("//")
   return is_slash_comment and not no_filetype
 end
 
 ---@return cmp_path.Option
 source._validate_option = function(_, params)
-  local option = vim.tbl_deep_extend('keep', params.option, defaults)
+  local option = vim.tbl_deep_extend("keep", params.option, defaults)
   vim.validate({
-    trailing_slash = { option.trailing_slash, 'boolean' },
-    label_trailing_slash = { option.label_trailing_slash, 'boolean' },
-    get_cwd = { option.get_cwd, 'function' },
+    trailing_slash = { option.trailing_slash, "boolean" },
+    label_trailing_slash = { option.label_trailing_slash, "boolean" },
+    get_cwd = { option.get_cwd, "function" },
   })
   return option
 end
 
 source._get_documentation = function(_, filename, count)
-  local binary = assert(io.open(filename, 'rb'))
+  local binary = assert(io.open(filename, "rb"))
   local first_kb = binary:read(1024)
-  if first_kb:find('\0') then
-    return { kind = cmp.lsp.MarkupKind.PlainText, value = 'binary file' }
+  if first_kb:find("\0") then
+    return { kind = cmp.lsp.MarkupKind.PlainText, value = "binary file" }
   end
 
   local contents = {}
@@ -219,12 +169,18 @@ source._get_documentation = function(_, filename, count)
 
   local filetype = vim.filetype.match({ filename = filename })
   if not filetype then
-    return { kind = cmp.lsp.MarkupKind.PlainText, value = table.concat(contents, '\n') }
+    return {
+      kind = cmp.lsp.MarkupKind.PlainText,
+      value = table.concat(contents, "\n"),
+    }
   end
 
-  table.insert(contents, 1, '```' .. filetype)
-  table.insert(contents, '```')
-  return { kind = cmp.lsp.MarkupKind.Markdown, value = table.concat(contents, '\n') }
+  table.insert(contents, 1, "```" .. filetype)
+  table.insert(contents, "```")
+  return {
+    kind = cmp.lsp.MarkupKind.Markdown,
+    value = table.concat(contents, "\n"),
+  }
 end
 
 return source
